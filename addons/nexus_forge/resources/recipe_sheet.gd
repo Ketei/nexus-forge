38e0b77f574d71d28b792d39c0b598ab11b94ac4
Ketei <ketei.ilust@gmail.com>
2025-11-17 20:29:37 -0600

    What the what

--- addons/nexus_forge/resources/recipe_sheet.gd
+++ addons/nexus_forge/resources/recipe_sheet.gd
@@ -1,2 +1,14 @@
 class_name RecipeSheet
 extends RefCounted
+
+## The ID of the recipe.
+var id: StringName = &""
+
+## The inputs of the recipe
+var input: Array[RecipeItem] = []
+
+## The outputs of the recipe
+var output: Array[RecipeItem] = []
+
+## The custom data of the recipe
+var data: Dictionary[String, Variant] = {}
