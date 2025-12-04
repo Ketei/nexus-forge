3f006cc7afd724504fabc16f1ede7cd2e7fb5647
Ketei <ketei.ilust@gmail.com>
2025-12-03 22:44:15 -0600

    Implemented item sorting on recipe item menu

--- addons/nexus_forge/recipes/recipe_items_tree_script.gd
+++ addons/nexus_forge/recipes/recipe_items_tree_script.gd
@@ -12,6 +12,7 @@ func _ready() -> void:
 	create_item()
 	set_column_title(0, "Item ID")
 	set_column_title(1, "Item Name")
+	column_title_clicked.connect(_on_column_title_clicked)
 
 
 func _get_drag_data(at_position: Vector2) -> Variant:
@@ -24,6 +25,27 @@ func _get_drag_data(at_position: Vector2) -> Variant:
 	set_drag_preview(item_label)
 	
 	return {"type": "item_id", "is_new": true, "item_id": StringName(node.get_text(0))}
+
+
+func _on_column_title_clicked(column: int, mouse_button_index: int) -> void:
+	if mouse_button_index != MOUSE_BUTTON_LEFT or column == sort_column:
+		return
+	
+	var items: Array[TreeItem] = get_root().get_children()
+	var item_count: int = items.size()
+	
+	if item_count < 2:
+		return
+	
+	items.sort_custom(
+			func(a:TreeItem,b:TreeItem): 
+				return a.get_text(sort_column).naturalnocasecmp_to( b.get_text(sort_column)) < 0 )
+	
+	if items[0].get_index() != 0:
+		items[0].move_before(get_root().get_first_child())
+	
+	for item_idx in range(1, item_count):
+		items[item_idx].move_after(items[item_idx - 1])
 
 
 func add_item(item_id: StringName, item_name: String) -> void:
