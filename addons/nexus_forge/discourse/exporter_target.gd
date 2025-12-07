aa5305060ea65750ce5ced0ef4ab155c502ae625
Ketei <ketei.ilust@gmail.com>
2025-12-06 23:04:58 -0600

    Object to help me quickly handle pointers

---
+++ addons/nexus_forge/discourse/exporter_target.gd
@@ -0,0 +1,25 @@
+extends RefCounted
+
+
+var anchor_nodes: Dictionary[StringName, StringName] = {}
+var dialog_mergers: Dictionary[StringName, StringName] = {}
+
+
+
+func get_target(uuid: StringName, _origin: StringName = &"", _iteration: int = 0) -> StringName:
+	if _origin == uuid or 100 <= _iteration:
+		if 100 <= _iteration:
+			printerr("Error: Over 99 anchor/joiner direct connections found. Breaking connection.\nAt this point, I'm pretty sure you're just drawing a picture of a worm. Please stop drawing worms.")
+		return &""
+	
+	if _origin.is_empty():
+		_origin = uuid
+	
+	if anchor_nodes.has(uuid):
+		#return _get_target.call(anchor_nodes[uuid], _origin, _iteration + 1)
+		return get_target(anchor_nodes[uuid], _origin, _iteration + 1)
+	elif dialog_mergers.has(uuid):
+		return get_target(dialog_mergers[uuid], _origin, _iteration + 1)
+		#return _get_target.call(dialog_mergers[uuid], _origin, _iteration + 1)
+	else:
+		return uuid
