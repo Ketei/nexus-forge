7f0a118d71d333543971377e9c2d2ae29d1e3ae7
Ketei <ketei.ilust@gmail.com>
2025-12-06 23:06:43 -0600

    Used ResourceLoader since FileAccess doesn't work on release due to Godot's remapping. Also clearing cache for leaked resources

--- addons/nexus_forge/classes/autoload/nexus_forge_singleton.gd
+++ addons/nexus_forge/classes/autoload/nexus_forge_singleton.gd
@@ -60,63 +60,63 @@ func _ready() -> void:
 	var recipe_path: String = ProjectSettings.get_setting(
 			"nexus_forge/recipes_path", "")
 	
-	if not blackboard_path.is_empty() and FileAccess.file_exists(blackboard_path):
+	if not blackboard_path.is_empty() and ResourceLoader.exists(blackboard_path):
 		var res_pre: Resource = load(blackboard_path)
 		if res_pre is BlackboardData:
 			Blackboard = res_pre
 		else:
 			printerr("[NEXUS FORGE] ProjectSettings: Invalid Blackboard.")
 	
-	if not stats_path.is_empty() and FileAccess.file_exists(stats_path):
+	if not stats_path.is_empty() and ResourceLoader.exists(stats_path):
 		var res_pre: Resource = load(stats_path)
 		if res_pre is StatCatalog:
 			Stats = res_pre
 		else:
 			printerr("[NEXUS FORGE] ProjectSettings: Invalid Stats.")
 	
-	if not traits_path.is_empty() and FileAccess.file_exists(traits_path):
+	if not traits_path.is_empty() and ResourceLoader.exists(traits_path):
 		var res_pre: Resource = load(traits_path)
 		if res_pre is TraitCatalog:
 			Traits = res_pre
 		else:
 			printerr("[NEXUS FORGE] ProjectSettings: Invalid Traits.")
 	
-	if not skill_path.is_empty() and FileAccess.file_exists(skill_path):
+	if not skill_path.is_empty() and ResourceLoader.exists(skill_path):
 		var res_pre: Resource = load(skill_path)
 		if res_pre is SkillCatalog:
 			Skills = res_pre
 		else:
 			printerr("[NEXUS FORGE] ProjectSettings: Invalid Skills.")
 	
-	if not quest_path.is_empty() and FileAccess.file_exists(quest_path):
+	if not quest_path.is_empty() and ResourceLoader.exists(quest_path):
 		var res_pre: Resource = load(quest_path)
 		if res_pre is QuestCatalog:
 			Quests = res_pre
 		else:
 			printerr("[NEXUS FORGE] ProjectSettings: Invalid Quests.")
 	
-	if not species_path.is_empty() and FileAccess.file_exists(species_path):
+	if not species_path.is_empty() and ResourceLoader.exists(species_path):
 		var res_pre: Resource = load(species_path)
 		if res_pre is SpeciesCatalog:
 			Species = res_pre
 		else:
 			printerr("[NEXUS FORGE] ProjectSettings: Invalid Species.")
 	
-	if not items_path.is_empty() and FileAccess.file_exists(items_path):
+	if not items_path.is_empty() and ResourceLoader.exists(items_path):
 		var res_pre: Resource = load(items_path)
 		if res_pre is ItemCatalog:
 			Items = res_pre
 		else:
 			printerr("[NEXUS FORGE] ProjectSettings: Invalid Items.")
 	
-	if not currency_path.is_empty() and FileAccess.file_exists(currency_path):
+	if not currency_path.is_empty() and ResourceLoader.exists(currency_path):
 		var res_pre: Resource = load(currency_path)
 		if res_pre is CurrencyCatalog:
 			Currency = res_pre
 		else:
 			printerr("[NEXUS FORGE] ProjectSettings: Invalid Currency.")
 	
-	if not recipe_path.is_empty() and FileAccess.file_exists(recipe_path):
+	if not recipe_path.is_empty() and ResourceLoader.exists(recipe_path):
 		var res_pre: Resource = load(recipe_path)
 		if res_pre is RecipeCatalog:
 			Recipes = res_pre
@@ -142,3 +142,7 @@ func _ready() -> void:
 	if Recipes == null:
 		Recipes = RecipeCatalog.new()
 	
+	
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_WM_CLOSE_REQUEST:
+		Discourse._clear_cache() # Clearing discourse cache to prevent leaked resources.
