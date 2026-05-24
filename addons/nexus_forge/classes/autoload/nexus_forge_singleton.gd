0204fba8c89999efcb14f583bd5f40908ab1f2b9
Ketei <ketei.ilust@gmail.com>
2026-05-24 15:02:53 -0600

    Objects will always instantiate regardless of disabled modules unless Nexus Forge/Settings/Instantiate Disabled Modules is set to off

--- addons/nexus_forge/classes/autoload/nexus_forge_singleton.gd
+++ addons/nexus_forge/classes/autoload/nexus_forge_singleton.gd
@@ -40,18 +40,16 @@ var _phrase_api: PhraseAPI = PhraseAPI.new()
 
 func _ready() -> void:
 	var use_discourse: bool = ProjectSettings.get_setting(EditorNFPlugin.get_project_settings_path("discourse_enabled"), true)
+	var use_items: bool = ProjectSettings.get_setting(EditorNFPlugin.get_project_settings_path("items_enabled"), true)
+	var use_talents: bool = ProjectSettings.get_setting(EditorNFPlugin.get_project_settings_path("talents_enabled"), true)
+	var use_species: bool = ProjectSettings.get_setting(EditorNFPlugin.get_project_settings_path("species_enabled"), true)
+	var use_quests: bool = ProjectSettings.get_setting(EditorNFPlugin.get_project_settings_path("quests_enabled"), true)
+	var use_currencies: bool = ProjectSettings.get_setting(EditorNFPlugin.get_project_settings_path("currencies_enabled"), true)
+	var use_recipes: bool = ProjectSettings.get_setting(EditorNFPlugin.get_project_settings_path("recipes_enabled"), true)
+	var instantiate_disabled: bool = ProjectSettings.get_setting(EditorNFPlugin.get_project_settings_path("use_disabled_modules"), true)
 	
 	var blackboard_path: String = ProjectSettings.get_setting(
 			EditorNFPlugin.get_project_settings_path("variables"), "")
-	var species_path: String = ProjectSettings.get_setting(
-			EditorNFPlugin.get_project_settings_path("species"), "")
-	var items_path: String = ProjectSettings.get_setting(
-			EditorNFPlugin.get_project_settings_path("items"), "")
-	var currency_path: String = ProjectSettings.get_setting(
-			EditorNFPlugin.get_project_settings_path("currency"), "")
-	var recipe_path: String = ProjectSettings.get_setting(
-			EditorNFPlugin.get_project_settings_path("recipes"), "")
-	
 	if not blackboard_path.is_empty() and ResourceLoader.exists(blackboard_path):
 		var res_pre: Resource = load(blackboard_path)
 		if res_pre is BlackboardData:
@@ -59,55 +57,75 @@ func _ready() -> void:
 		else:
 			printerr("[NEXUS FORGE] ProjectSettings: Invalid Blackboard.")
 	
-	if not species_path.is_empty() and ResourceLoader.exists(species_path):
-		var res_pre: Resource = load(species_path)
-		if res_pre is SpeciesCatalog:
-			Species = res_pre
-		else:
-			printerr("[NEXUS FORGE] ProjectSettings: Invalid Species.")
+	if use_species:
+		var species_path: String = ProjectSettings.get_setting(
+				EditorNFPlugin.get_project_settings_path("species"), "")
+		if not species_path.is_empty() and ResourceLoader.exists(species_path):
+			var res_pre: Resource = load(species_path)
+			if res_pre is SpeciesCatalog:
+				Species = res_pre
+			else:
+				printerr("[NEXUS FORGE] ProjectSettings: Invalid Species.")
 	
-	if not items_path.is_empty() and ResourceLoader.exists(items_path):
-		var res_pre: Resource = load(items_path)
-		if res_pre is ItemCatalog:
-			Items = res_pre
-		else:
-			printerr("[NEXUS FORGE] ProjectSettings: Invalid Items.")
+	if use_items:
+		var items_path: String = ProjectSettings.get_setting(
+				EditorNFPlugin.get_project_settings_path("items"), "")
+		if not items_path.is_empty() and ResourceLoader.exists(items_path):
+			var res_pre: Resource = load(items_path)
+			if res_pre is ItemCatalog:
+				Items = res_pre
+			else:
+				printerr("[NEXUS FORGE] ProjectSettings: Invalid Items.")
 	
-	if not currency_path.is_empty() and ResourceLoader.exists(currency_path):
-		var res_pre: Resource = load(currency_path)
-		if res_pre is CurrencyCatalog:
-			Currency = res_pre
-		else:
-			printerr("[NEXUS FORGE] ProjectSettings: Invalid Currency.")
+	if use_currencies:
+		var currency_path: String = ProjectSettings.get_setting(
+				EditorNFPlugin.get_project_settings_path("currency"), "")
+		if not currency_path.is_empty() and ResourceLoader.exists(currency_path):
+			var res_pre: Resource = load(currency_path)
+			if res_pre is CurrencyCatalog:
+				Currency = res_pre
+			else:
+				printerr("[NEXUS FORGE] ProjectSettings: Invalid Currency.")
 	
-	if not recipe_path.is_empty() and ResourceLoader.exists(recipe_path):
-		var res_pre: Resource = load(recipe_path)
-		if res_pre is RecipeCatalog:
-			Recipes = res_pre
-		else:
-			printerr("[NEXUS FORGE] ProjectSettings: Invalid Recipes.")
+	if use_recipes:
+		var recipe_path: String = ProjectSettings.get_setting(
+				EditorNFPlugin.get_project_settings_path("recipes"), "")
+		if not recipe_path.is_empty() and ResourceLoader.exists(recipe_path):
+			var res_pre: Resource = load(recipe_path)
+			if res_pre is RecipeCatalog:
+				Recipes = res_pre
+			else:
+				printerr("[NEXUS FORGE] ProjectSettings: Invalid Recipes.")
 	
 	if Discourse == null:
-		Discourse = EditorDialogParser.new()
+		if instantiate_disabled or use_discourse:
+			Discourse = EditorDialogParser.new()
+			if use_discourse:
+				Discourse.generate_locale_map()
+	else:
 		if use_discourse:
 			Discourse.generate_locale_map()
+
 	if Blackboard == null:
 		Blackboard = BlackboardData.new()
-	if Stats == null:
-		Stats = StatCatalog.new()
-	if Traits == null:
-		Traits = TraitCatalog.new()
-	if Skills == null:
-		Skills = SkillCatalog.new()
-	if Quests == null:
+	
+	if use_talents or instantiate_disabled:
+		if Stats == null:
+			Stats = StatCatalog.new()
+		if Traits == null:
+			Traits = TraitCatalog.new()
+		if Skills == null:
+			Skills = SkillCatalog.new()
+	
+	if Quests == null and ( use_quests or instantiate_disabled ):
 		Quests = QuestManager.new()
-	if Species == null:
+	if Species == null and ( use_species or instantiate_disabled ):
 		Species = SpeciesCatalog.new()
-	if Items == null:
+	if Items == null and ( use_items or instantiate_disabled):
 		Items = ItemCatalog.new()
-	if Currency == null:
+	if Currency == null and ( use_currencies or instantiate_disabled ):
 		Currency = CurrencyCatalog.new()
-	if Recipes == null:
+	if Recipes == null and ( use_recipes or instantiate_disabled ):
 		Recipes = RecipeCatalog.new()
 	
 	
