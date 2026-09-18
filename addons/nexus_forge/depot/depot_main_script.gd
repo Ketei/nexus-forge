f143c63b002c336435874a62aecfb58a6a38664c
Ketei <ketei.ilust@gmail.com>
2026-09-18 16:30:32 -0600

    Changed the order to pass the object before any calls to it

--- addons/nexus_forge/depot/depot_main_script.gd
+++ addons/nexus_forge/depot/depot_main_script.gd
@@ -42,11 +42,13 @@ func _input(event: InputEvent) -> void:
 
 func ready_plugin(use_items: bool, use_currencies: bool, paths_resource: RefCounted) -> void:
 	set_process_input(true)
+	if use_items:
+		items_container._script_paths = paths_resource
 	items_container.ready_plugin(use_items, use_currencies, MAX_UNDO_STEPS)
+		
 	if use_items:
 		if items_container.item_link.items != null:
 			categories_container.items_resource = items_container.item_link.items
-		items_container._script_paths = paths_resource
 		categories_container.ready_plugin(MAX_UNDO_STEPS)
 	
 	items_container.visible = true
