d18c6ae1cacefd86088c457fa2a4ac75aa6fe0f5
Ketei <ketei.ilust@gmail.com>
2026-09-24 14:00:32 -0600

    Fixed an infinite loop on NFArrayUtils.erase_all

--- addons/nexus_forge/classes/utils/array_utils.gd
+++ addons/nexus_forge/classes/utils/array_utils.gd
@@ -268,6 +268,7 @@ static func erase_all(array: Array, value: Variant) -> void:
 	var idx: int = array.rfind(value)
 	while 0 <= idx:
 		array.remove_at(idx)
+		idx = array.rfind(value)
 
 
 ## Checks if the [param index] is within bounds of the [param array],
